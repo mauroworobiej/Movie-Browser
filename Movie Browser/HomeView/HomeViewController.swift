e0a4f4bac9d0e688a17f439529602130a935771f
Mauro Worobiej <mauro.worobiej@gmail.com>
2020-09-23 10:43:14 -0300

    Add Table View to Home VC

--- Movie Browser/HomeView/HomeViewController.swift
+++ Movie Browser/HomeView/HomeViewController.swift
@@ -9,11 +9,57 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    
+    // MARK:- Properties
+    
+    lazy var tableView: UITableView = {
+        let table = UITableView()
+        table.dataSource = self
+        table.delegate = self
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "moviewCell")
+        table.translatesAutoresizingMaskIntoConstraints = false
+        return table
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .red
+        setupLayout()
     }
+    
+    // MARK:- Helper Methods
+    func setupLayout() {
+        view.addSubview(tableView)
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+        
+    }
+
+}
+
+// MARK:- Table view Data Source
+
+extension HomeViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "moviewCell", for: indexPath)
+        cell.textLabel?.text = "Testing cell"
+        return cell
+    }
+    
+    
+}
+
+// MARK:- Table view Delegate
+
+extension HomeViewController: UITableViewDelegate {
     
 }
