8d9ebe2451b700897339a3adff63896c0b51b8a6
Mauro Worobiej <mauro.worobiej@gmail.com>
2020-09-23 10:20:00 -0300

    Delete Main Storyboard
    Add Home VC in order to create view programatically.
    
    Instantiet from sceneDelegate

--- Movie Browser/HomeView/HomeViewController.swift
+++ Movie Browser/HomeView/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  Movie Browser
 //
 //  Created by Mauro Worobiej on 23/09/2020.
@@ -8,13 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .red
     }
-
-
+    
 }
-
