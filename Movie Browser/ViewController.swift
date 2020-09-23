992a26f201cdff6a9ed7156eb398c640da21df28
Mauro Worobiej <mauro.worobiej@gmail.com>
2020-09-23 10:06:42 -0300

    First commit

---
+++ Movie Browser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movie Browser
+//
+//  Created by Mauro Worobiej on 23/09/2020.
+//  Copyright © 2020 Mauro Worobiej. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
